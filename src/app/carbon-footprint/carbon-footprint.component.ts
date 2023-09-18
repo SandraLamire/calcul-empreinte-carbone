1d686be85a4b72644e302a80e8abc17daa7db920
SandraLamire <sandra.lamire.sl@gmail.com>
2023-09-18 22:57:41 +0200

    Ajout Header + Body + Footer

---
+++ src/app/carbon-footprint/carbon-footprint.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carbon-footprint',
+  templateUrl: './carbon-footprint.component.html',
+  styleUrls: ['./carbon-footprint.component.css']
+})
+export class CarbonFootprintComponent {
+
+}
