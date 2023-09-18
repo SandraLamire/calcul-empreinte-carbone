92b0584680c87e11a1b2fe7ea7fe185a6e8e7510
SandraLamire <sandra.lamire.sl@gmail.com>
2023-09-18 23:03:45 +0200

    Utilisation des différents hooks

--- src/app/carbon-footprint/carbon-footprint.component.ts
+++ src/app/carbon-footprint/carbon-footprint.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './carbon-footprint.component.html',
   styleUrls: ['./carbon-footprint.component.css']
 })
-export class CarbonFootprintComponent {
 
+// Utiliser les différents hooks pour afficher dans la console 
+// ( navigateur => F12 => Console => Journaux )
+// le cycle de vie du composant
+export class CarbonFootprintComponent {
+  ngOnInit() {
+    console.log('Le composant a été initialisé.');
+  }
+
+  ngOnDestroy() {
+    console.log('Le composant a été détruit.');
+  }
+
+  ngAfterContentInit() {
+    console.log('Le contenu du composant a été initialisé.');
+  }
+
+  ngAfterContentChecked() {
+    console.log('Le contenu du composant est vérifié.');
+  }
+
+  ngAfterViewInit() {
+    console.log('La vue du composant a été initialisée.');
+  }
+
+  ngAfterViewChecked() {
+    console.log('La vue du composant a été vérifiée.');
+  }
 }
