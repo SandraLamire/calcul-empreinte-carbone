08456a6aed8abc79752e5bfe8deab38eaf7ed3fb
SandraLamire <sandra.lamire.sl@gmail.com>
2023-09-19 21:17:30 +0200

    Ajout quantité CO2

--- src/app/carbon-footprint/carbon-footprint.component.ts
+++ src/app/carbon-footprint/carbon-footprint.component.ts
@@ -10,62 +10,36 @@ import { CarbonFootprintComputeService } from '../services/carbon-footprint-comp
 
 export class CarbonFootprintComponent {
 
-    // ajout des attributs distance et consommation
+    // ajout des attributs distance, consommation et quantité CO2
   distanceKm: number = 150;
   consommationPour100Km: number = 5;
+  quantiteCO2Totale: number = 0;
 
   ajouter100Km() {
     this.distanceKm += 100;
   }
 
-  /* Tableau déplacé dans services 
-  voyages = [
-        { distanceKm: 50, consommationPour100Km: 5 },
-        { distanceKm: 150, consommationPour100Km: 6 },
-        { distanceKm: 250, consommationPour100Km: 7 },
-        { distanceKm: 350, consommationPour100Km: 8 },
-        { distanceKm: 450, consommationPour100Km: 9 }
-    ];
- 
-  constructor() {
-      this.calculerTotalEtMoyenne();
-  }
-  */
-
   /* récupération du tableau de données voyages dans services */
   constructor(private carbonFootprintComputeService: CarbonFootprintComputeService) { };
-
   voyages = this.carbonFootprintComputeService.getVoyages();
 
-
- 
     // Appel à la méthode addVoyage(voyage)
     genererVoyage() {
       const distance = Math.floor(Math.random() * 1000) + 1;
       const consommation = Math.floor(Math.random() * 10) + 1;
-      /* Déplacé dans addVoyage dans services
-      this.voyages.push({ distanceKm: distance, consommationPour100Km: consommation });
-      */
+      
       this.carbonFootprintComputeService.addVoyage({ distanceKm: distance, consommationPour100Km: consommation });
       this.voyages = this.carbonFootprintComputeService.getVoyages();
       this.calculerTotalEtMoyenne();
     };
 
     calculerTotalEtMoyenne() {
-      /* Déplacé-Modifié dans services
-      let total = 0;
-      let moyenne = 0;
-      for (const voyage of this.voyages) {
-        total += voyage.distanceKm;
-        moyenne += voyage.consommationPour100Km;
-      }
-      this.distanceKm = total;
-      this.consommationPour100Km = moyenne / this.voyages.length;
-      */
       let resume = this.carbonFootprintComputeService.getResumeVoyages();
       this.distanceKm = resume.distanceKm;
       this.consommationPour100Km = resume.moyenneConsommation;
+      this.quantiteCO2Totale = resume.quantiteCO2Totale;
     }
+
 
 
   // Utiliser les différents hooks pour afficher dans la console 
--- src/app/services/carbon-footprint-compute.service.ts
+++ src/app/services/carbon-footprint-compute.service.ts
@@ -8,11 +8,11 @@ export class CarbonFootprintComputeService {
   constructor() { }
 
   private voyages = [
-    { distanceKm: 100, consommationPour100Km: 5.5 },
-    { distanceKm: 200, consommationPour100Km: 6.5 },
-    { distanceKm: 300, consommationPour100Km: 7.5 },
-    { distanceKm: 400, consommationPour100Km: 8.5 },
-    { distanceKm: 500, consommationPour100Km: 9.5 },
+    { distanceKm: 100, consommationPour100Km: 5.5, quantiteCO2: 2.39 },
+    { distanceKm: 200, consommationPour100Km: 6.5, quantiteCO2: 5.65 },
+    { distanceKm: 300, consommationPour100Km: 7.5, quantiteCO2: 9.78 },
+    { distanceKm: 400, consommationPour100Km: 8.5, quantiteCO2: 14.78 },
+    { distanceKm: 500, consommationPour100Km: 9.5, quantiteCO2: 20.65 },
   ];
 
   getVoyages() {
@@ -20,6 +20,7 @@ export class CarbonFootprintComputeService {
   }
 
   addVoyage(voyage: any) {
+    voyage.quantiteCO2 = (voyage.distanceKm * voyage.consommationPour100Km) / 100 * 2.3;
     this.voyages.push(voyage);
   }
 
@@ -27,11 +28,14 @@ export class CarbonFootprintComputeService {
     let distanceKm = 0;
     let consommationPour100Km = 0;
     let moyenneConsommation = 0;
+    let quantiteCO2Totale = 0;
+
     this.voyages.forEach(voyage => {
       distanceKm += voyage.distanceKm;
       consommationPour100Km += voyage.consommationPour100Km;
       moyenneConsommation = consommationPour100Km / this.voyages.length;
+      quantiteCO2Totale += voyage.quantiteCO2;
     });
-    return { distanceKm, consommationPour100Km, moyenneConsommation };
+    return { distanceKm, consommationPour100Km, moyenneConsommation, quantiteCO2Totale };
   } 
 }
